2660902e180a710fd3b19c3c1dd82668efd08d43
nulld <krawdev@gmail.com>
2024-01-05 00:03:44 -0300

    feat: IFuncionarioInterface

---
+++ web-api/Service/FuncionarioService/IFuncionarioInterface.cs
@@ -0,0 +1,14 @@
+﻿using web_api.Models;
+
+namespace web_api.Service.FuncionarioService
+{
+    public interface IFuncionarioInterface
+    {
+        Task<ServiceResponse<List<FuncionarioModel>>> GetFuncionarios();
+        Task<ServiceResponse<List<FuncionarioModel>>> CreateFuncionario(FuncionarioModel novoFuncionario);
+        Task<ServiceResponse<FuncionarioModel>> GetFuncionario(int id);
+        Task<ServiceResponse<List<FuncionarioModel>>> UpdateFuncionario(FuncionarioModel editadoFuncionario);
+        Task<ServiceResponse<List<FuncionarioModel>>> DeleteFuncionario(int id);
+        Task<ServiceResponse<List<FuncionarioModel>>> InativaFuncionario(int id);
+    }
+}
