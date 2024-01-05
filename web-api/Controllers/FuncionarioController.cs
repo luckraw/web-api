2ea9beddcc266d2aabeb09c51d3d57f9266061fe
nulld <krawdev@gmail.com>
2024-01-05 00:47:47 -0300

    feat: FuncionarioService, HttpGet Controller impl.

---
+++ web-api/Controllers/FuncionarioController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using web_api.Models;
+using web_api.Service.FuncionarioService;
+
+namespace web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FuncionarioController : ControllerBase
+    {
+        private readonly IFuncionarioInterface _funcionarioInterface;
+
+        public FuncionarioController(IFuncionarioInterface funcionarioInterface)
+        {
+            
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<FuncionarioModel>>>> GetFuncionarios()
+        {
+            return Ok(await _funcionarioInterface.GetFuncionarios());
+
+        }
+    }
+}
