108ff619c67576d03708dccea19077a9a89783d1
nulld <krawdev@gmail.com>
2024-01-04 21:37:51 -0300

    feat: ServiceResponse

--- web-api/Models/ServiceResponse.cs
+++ web-api/Models/ServiceResponse.cs
@@ -1,6 +1,10 @@
 ﻿namespace web_api.Models
 {
-    public class ServiceResponse
+    public class ServiceResponse<T>
     {
+        public T? Dados  { get; set; }
+        public string Mensagem { get; set; } = string.Empty;
+        public bool Sucesso { get; set; }
+
     }
 }
